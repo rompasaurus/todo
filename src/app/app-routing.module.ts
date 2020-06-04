74f05cfa97761aa31ac48a92e99158314e485768
rompasaurus <michael.e.mchenry@gmail.com>
2020-06-04 11:38:28 -0400

    Finished angular section of udemy coursework

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,14 @@ import {WelcomeComponent} from './welcome/welcome.component';
 import {ErrorComponent} from './error/error.component';
 import {ListTodosComponent} from './list-todos/list-todos.component';
 import {LogoutComponent} from "./logout/logout.component";
+import {RouteGuardService} from "./service/route-guard.service";
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
-  {path: 'welcome/:name', component: WelcomeComponent},
+  {path: 'welcome/:name', component: WelcomeComponent, canActivate: [RouteGuardService]},
   {path: 'login', component: LoginComponent},
-  {path: 'logout', component: LogoutComponent},
-  {path: 'todos', component: ListTodosComponent},
+  {path: 'logout', component: LogoutComponent, canActivate: [RouteGuardService]},
+  {path: 'todos', component: ListTodosComponent, canActivate: [RouteGuardService]},
   {path: '**', component: ErrorComponent}
 ];
 
--- src/app/service/route-guard.service.ts
+++ src/app/service/route-guard.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree} from "@angular/router";
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from "@angular/router";
+import {HardcodedAuthenticationService} from "./hardcoded-authentication.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class RouteGuardService implements CanActivate{
 
-  constructor() { }
+
+  constructor(private hardcodedAuthenticationService: HardcodedAuthenticationService, private router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
-
-    return true;
+    if (this.hardcodedAuthenticationService.isUserLoggedIn()) {
+      return true;
+    }
+    this.router.navigate(['login']);
+    return false;
   }
 }
